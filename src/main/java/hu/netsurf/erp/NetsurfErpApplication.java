d826fd91f07468aae26daa0e99ec2977cdbfafb5
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-02-05 20:43:47 +0100

    Initial commit.

---
+++ src/main/java/hu/netsurf/erp/NetsurfErpApplication.java
@@ -0,0 +1,13 @@
+package hu.netsurf.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetsurfErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetsurfErpApplication.class, args);
+    }
+
+}
