cd7dea305a48877d70f6b22e465b0535e9843701
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-02-05 20:59:25 +0100

    Rename main class and it's unit test class.

--- src/main/java/hu/netsurf/erp/Application.java
+++ src/main/java/hu/netsurf/erp/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NetsurfErpApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(NetsurfErpApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
---
+++ src/test/java/hu/netsurf/erp/ApplicationTests.java
@@ -0,0 +1,10 @@
+package hu.netsurf.erp;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
